5a5688acc0171070cbcf7673c03e15324608bd22
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-09-23 00:19:30 +0300

    add header desctop

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
-    
+    $(".menu-vertical__name").click(function () {
+        if ($(this).next('.menu-vertical__list').css("display") == "none") {
+            $(this).next('.menu-vertical__list').slideDown();
+        }
+        else {
+            $(this).next('.menu-vertical__list').slideUp();
+        }
+    });
+
+       
 
 });
